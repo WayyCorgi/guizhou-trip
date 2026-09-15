40bf497c2b66d71dfcf6cfa6efff6e93f3d5488e
zhonggen <zhonggen@staff.weibo.com>
2026-09-15 23:00:02 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp ~/Programs/vue/guizhou-trip.html ~/Programs/vue/guizhou-trip/index.html
 cp ~/Programs/vue/route-map.jpeg ~/Programs/vue/guizhou-trip/route-map.jpeg 2>/dev/null
-cd ~/Programs/vue/guizhou-trip && git add . && git commit -m "update" && git push
+cd ~/Programs/vue/guizhou-trip && git add . && git commit -m "update" && git -c http.version=HTTP/1.1 push
