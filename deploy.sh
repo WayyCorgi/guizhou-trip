10b5a3edc1e3f30aff7ce72ab2534329dbdd45b7
zhonggen <zhonggen@staff.weibo.com>
2026-09-19 16:50:45 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp ~/Programs/vue/guizhou-trip.html ~/Programs/vue/guizhou-trip/index.html
 cp ~/Programs/vue/route-map.jpeg ~/Programs/vue/guizhou-trip/route-map.jpeg 2>/dev/null
-cp ~/Programs/vue/guizhou-trip/fanjingshan-guide.jpg ~/Programs/vue/guizhou-trip/fanjingshan-guide.jpg 2>/dev/null
-# Add new images here: cp /path/to/image.jpg ~/Programs/vue/guizhou-trip/
+cp ~/Programs/vue/fanjingshan-guide.jpg ~/Programs/vue/guizhou-trip/fanjingshan-guide.jpg 2>/dev/null
+cp ~/Programs/vue/huangguoshu-guide.jpg ~/Programs/vue/guizhou-trip/huangguoshu-guide.jpg 2>/dev/null
 cd ~/Programs/vue/guizhou-trip && git add . && git commit -m "update" && git -c http.version=HTTP/1.1 push
